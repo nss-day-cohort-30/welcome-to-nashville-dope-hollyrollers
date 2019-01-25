244c0e1402b655bcf70e565fb858a651f5f7ebfa
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-01-25 09:48:27 -0600

    fixed typo

--- parks/parks.js
+++ parks/parks.js
@@ -1,8 +1,18 @@
+let parksSearchButton = document.getElementById("searchParkBTN")
+
+parksSearchButton.addEventListener("click" )
+
+
+
 fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=lK9BzlilnoiwMiVKvT4z4jL5G")
 .then(response => response.json())
 .then( entireParkList => {
+    let counter = 0
     entireParkList.forEach( park => { 
-        let parkNames = park.park_name
-    console.log(parkNames)
+        if (park.hasOwnProperty('dog_park') && park.dog_park === "Yes"){
+            counter++
+            console.log(park.park_name)
+        }
     })
+    console.log(counter)
 })
