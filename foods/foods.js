d7dcc05720e0f8758e2682d0d4af843671055272
Stephen Clark <stephenc369@gmail.com>
2019-01-25 18:17:28 -0600

    for loop adds buttons with unique id

--- foods/foods.js
+++ foods/foods.js
@@ -5,38 +5,38 @@ const fetchAll = () => {
         .then(rsts => rsts.json())
         .then(parsedRestaurants => {
             const restArray = parsedRestaurants[Object.keys(parsedRestaurants)[3]]
-            //  const thirdRest = (restArray[2]["restaurant"])
+            const thirdRest = (restArray[2]["restaurant"])
         })
 }
 
 const addToDom = (restaurant) => {
     resultOutput.innerHTML += `${restaurant.name}: `
     resultOutput.innerHTML += `${restaurant.location.address}  `
-    const button = document.createElement("button");
-    button.setAttribute("id", "1");
+}
+const createButton = (index) => {
+    const button = document.createElement("button")
+    button.setAttribute("id", [index])
     button.appendChild(document.createTextNode("Add to your itinerary"));
     resultOutput.appendChild(button);
-
 }
 const fetchCuisine = (cuisine_id) => {
     fetch(`https://developers.zomato.com/api/v2.1/search?city_id=1138&cuisines=${cuisine_id}&apikey=812fea37dd42b5c8a532f151eaf25416`)
         .then(result => result.json())
         .then(parsedResult => {
             const searchedArray = parsedResult[Object.keys(parsedResult)[3]]
-            // const firstRest = (searchedArray[0]["restaurant"])
-            searchedArray.forEach(element => {
+                for (let index = 0; index < searchedArray.length; index++) {
+                const element = searchedArray[index]
                 const eachRestaurant = element["restaurant"]
                 addToDom(eachRestaurant)
-            });
+                createButton(index)
+              }
 
-        })
+        });
+
 }
 
 const searchButton = document.querySelector("#searchRestaurantBTN")
 searchButton.addEventListener("click", function () {
     let cuisineInput = document.querySelector("#restOptions").value
-    console.log(document.querySelector("#restOptions"))
-    // if (querySelector(section[name = "Restaurant Options"])=== )
-
     fetchCuisine(cuisineInput)
 })
