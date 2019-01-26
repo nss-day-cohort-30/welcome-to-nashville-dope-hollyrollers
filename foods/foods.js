fc056c389d191ee84f35a2509ae79c9853b27288
Stephen Clark <stephenc369@gmail.com>
2019-01-25 21:36:33 -0600

    create div wrappers with ids matching button ids

--- foods/foods.js
+++ foods/foods.js
@@ -1,42 +1,56 @@
 const resultOutput = document.querySelector("#resultsContainer")
 
-const fetchAll = () => {
-    fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=812fea37dd42b5c8a532f151eaf25416")
-        .then(rsts => rsts.json())
-        .then(parsedRestaurants => {
-            const restArray = parsedRestaurants[Object.keys(parsedRestaurants)[3]]
-            const thirdRest = (restArray[2]["restaurant"])
-        })
-}
-const addToDom = (restaurant) => {
-    resultOutput.innerHTML += `${restaurant.name}: `
-    resultOutput.innerHTML += `${restaurant.location.address}  `
-}
-const createButton = (index) => {
+//need to break this up
+const createRestHTML = (restaurant, index) => {
+    const restContainer = document.createElement("div")
+    restContainer.setAttribute("id", index)
+    const restName = document.createElement("h4")
+    const nameContent = document.createTextNode(restaurant.name)
+    restName.appendChild(nameContent)
+    const restAddress = document.createElement("span")
+    const addressContent = document.createTextNode(restaurant.location.address)
+    restAddress.appendChild(addressContent)
     const button = document.createElement("button")
     button.setAttribute("id", [index])
-    button.appendChild(document.createTextNode("Add to your itinerary"));
-    resultOutput.appendChild(button);
+    button.appendChild(document.createTextNode("Add to your itinerary"))
+    resultOutput.appendChild(restContainer)
+    restContainer.appendChild(restName)
+    restContainer.appendChild(restAddress)
+    restContainer.appendChild(button)
+
+}
+const addToDom = (restaurant, index) => {
+
 }
 
 
+
+const addEvents = resultOutput.addEventListener("click", function () {
+    event.target
+
+})
+
+//fetches all restaurants matching the chosen cuisine and adds them to the DOM with associated buttons
 const fetchCuisine = (cuisine_id) => {
     fetch(`https://developers.zomato.com/api/v2.1/search?city_id=1138&cuisines=${cuisine_id}&apikey=812fea37dd42b5c8a532f151eaf25416`)
         .then(result => result.json())
         .then(parsedResult => {
-            const searchedArray = parsedResult[Object.keys(parsedResult)[3]]
-                for (let index = 0; index < searchedArray.length; index++) {
+            const restArray = parsedResult[Object.keys(parsedResult)[3]]
+            for (let index = 0; index < restArray.length; index++) {
                 const element = searchedArray[index]
                 const eachRestaurant = element["restaurant"]
-                addToDom(eachRestaurant)
-                createButton(index)
-              }
+                createRestHTML(eachRestaurant, index)
+                addToDom(eachRestaurant, index)
+
+            }
 
         });
 }
+
+//When the search button is clicked, fetch restaurants matching the selected cuisine
 const searchButton = document.querySelector("#searchRestaurantBTN")
 searchButton.addEventListener("click", function () {
     resultOutput.innerHTML = ""
-    let cuisineInput = document.querySelector("#restOptions").value
-    fetchCuisine(cuisineInput)
+    let cuisine_id = document.querySelector("#restOptions").value
+    fetchCuisine(cuisine_id)
 })
